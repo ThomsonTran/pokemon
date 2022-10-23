3d1f67f3eef5071caad0e54cbe6bad90206a6310
Tom <thomson.tran@gmail.com>
2022-10-23 15:54:59 -0400

    seed db with data from pokemon csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib','seeds','pokemon.csv')) 
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+    t = Monster.new
+    t.number = row['#']
+    t.name = row['Name']
+    t.type_1 = row['Type 1']
+    t.type_2 = row['Type 2']
+    t.total = row['Total']
+    t.hp = row['HP']
+    t.attack = row['Attack']
+    t.defense = row['Defense']
+    t.sp_atk = row['Sp. Atk']
+    t.sp_def = row['Sp. Def']
+    t.speed = row['Speed']
+    t.generation = row['Generation']
+    t.legendary = row['Legendary']
+    t.save
+    puts "#{t.name}, #{t.number} saved"
+    
+end
+
+p "created #{Monster.count} rows"
+
